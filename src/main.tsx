5b8a0523a8e5c28aa4d5ba98672f3a478bce2170
Dang Thuyen Vuong <dangthuyenvuong@gmail.com>
2023-05-02 17:52:05 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -6,21 +6,38 @@ import { Auth0Provider } from '@auth0/auth0-react'
 import { AuthProvider } from '@components/AuthProvider'
 import '@assets/tailwind.css'
 import { BrowserRouter } from 'react-router-dom'
+import { ConfigProvider } from 'antd'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	// <React.StrictMode>
-	<BrowserRouter>
-		<Auth0Provider
-			domain='dev-te6zolyvwhgh31vx.us.auth0.com'
-			clientId='hHsE8rgYG5czcJsV6bBnbPjk4U5q8jIc'
-			authorizationParams={{
-				redirect_uri: window.location.origin,
-			}}
-		>
-			<AuthProvider>
-				<App />
-			</AuthProvider>
-		</Auth0Provider>
-	</BrowserRouter>,
+	<ConfigProvider
+		theme={{
+			token: {
+				colorPrimary: '#00b96b',
+			},
+		}}
+		form={{
+			validateMessages: {
+				required: '"${label}" là bắt buộc',
+				types: {
+					email: '${label}',
+				},
+			},
+		}}
+	>
+		<BrowserRouter>
+			<Auth0Provider
+				domain='dev-te6zolyvwhgh31vx.us.auth0.com'
+				clientId='hHsE8rgYG5czcJsV6bBnbPjk4U5q8jIc'
+				authorizationParams={{
+					redirect_uri: window.location.origin,
+				}}
+			>
+				<AuthProvider>
+					<App />
+				</AuthProvider>
+			</Auth0Provider>
+		</BrowserRouter>
+	</ConfigProvider>,
 	// </React.StrictMode>,
 )
